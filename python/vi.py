046d36d583bd1401426e47aa71d1f2d27265f8ae
yunlong <yun-long.song@outlook.com>
2019-01-24 07:17:11 +0100

    change my handsome profile picutre

--- python/vi.py
+++ python/vi.py
@@ -22,35 +22,44 @@ import numpy as np
 # axes[2].set_title("$t(x)$")
 # plt.show()
 
-# # Second plot, norm distribution
-# fig, ax = plt.subplots(1,1, figsize=(6, 3))
-# mu = 0
-# variance = 1
-# sigma = np.sqrt(variance)
-# x = np.linspace(mu-12*sigma, mu+12*sigma, 100)
-# ax.plot(x, mlab.normpdf(x, mu, sigma), label='$p(x) \sim \mathcal{N}(0, 1)$')
-# ax.plot(x, mlab.normpdf(x, mu+2, 2*sigma), label='$q(x) \sim \mathcal{N}(2, 2)$')
-# # ax.plot(x, mlab.normpdf(x, mu, 4*sigma))
-# plt.legend()
-# plt.show()
+# Second plot, norm distribution
+fig, ax = plt.subplots(1,1, figsize=(4, 2))
+mu = 0
+variance = 1
+sigma = np.sqrt(variance)
+x = np.linspace(mu-20*sigma, mu+20*sigma, 100)
+y0 = np.zeros(len(x))
+y1 = mlab.normpdf(x, mu+6, sigma)
+y2 = mlab.normpdf(x, mu-4, 2*sigma)
+yp = 0.8 * y1 + 0.2 * y2
+yq = mlab.normpdf(x, mu+1 , 3 * sigma)
+ax.plot(x, yp, label=r'$p(x)$', color='b')
+ax.fill_between(x, y0, yp, color='b', alpha=0.3)
+ax.plot(x, yq, label=r'$q_{\theta}(x)$', color='r')
+ax.fill_between(x, y0, yq, color='r', alpha=0.3)
+ax.axis("off")
+# ax.plot(x, mlab.normpdf(x, mu-4, 4*sigma), label='$q(x) \sim \mathcal{N}(2, 2)$')
+# ax.plot(x, mlab.normpdf(x, mu, 4*sigma))
+plt.legend()
+plt.show()
 
 # # Third plot, Jensen's inequality
-fig, axes = plt.subplots(1, 2, figsize=(8, 3))
-x = np.linspace(0, 10, 100)
-y1 = (x - 5)**2
-y2 = x + 3
-y3 = np.log(x)
-y4 = 0.5 * x - 1
-axes[0].plot(x, y1)
-axes[0].plot(x, y2)
-axes[0].axis("off")
-axes[0].set_title("Convex")
-axes[1].plot(x, y3)
-axes[1].plot(x, y4)
-axes[1].axis("off")
-axes[1].set_title("Concave")
-fig.patch.set_visible(False)
-plt.show()
+# fig, axes = plt.subplots(1, 2, figsize=(8, 3))
+# x = np.linspace(0, 10, 100)
+# y1 = (x - 5)**2
+# y2 = x + 3
+# y3 = np.log(x)
+# y4 = 0.5 * x - 1
+# axes[0].plot(x, y1)
+# axes[0].plot(x, y2)
+# axes[0].axis("off")
+# axes[0].set_title("Convex")
+# axes[1].plot(x, y3)
+# axes[1].plot(x, y4)
+# axes[1].axis("off")
+# axes[1].set_title("Concave")
+# fig.patch.set_visible(False)
+# plt.show()
 
 
 
